62a2ddfc51db885ca71bc0c43ff601a1de2712e9
jacen green <jacen.l.green@hotmail.com>
2024-06-30 14:43:52 +0100

    REFACTOR: Overhaul and simplify booking model

--- booking/admin.py
+++ booking/admin.py
@@ -1,9 +1,41 @@
 from django.contrib import admin
-from .models import TimeSlot, Table, TableSlot, Package, Booking
+from .models import Table, Package, Booking
 
-# Register your models here.
-admin.site.register(TimeSlot)
-admin.site.register(Table)
-admin.site.register(TableSlot)
-admin.site.register(Package)
-admin.site.register(Booking)
+# Registry code adapted from Mentor Gareth McGirr
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    """ Admin panel view of tables capacity filter. """
+    list_display = (
+        'number',
+        'capacity',
+    )
+    list_filter = ('capacity',)
+
+
+@admin.register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    """ Admin panel view of created packages. """
+    list_display = (
+        'title',
+    )
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    """ Admin panel view of bookings using date range filter package. """
+    list_display = (
+        'pk',
+        'booking_date',
+        'booking_slot',
+        'booking_name',
+        'booking_table',
+        'booking_size',
+        'event',
+        'allergies',
+    )
+    list_filter = (
+        'booking_slot', 'booking_table',
+    )
+
+# admin.site.register(Booking)
--- booking/models.py
+++ booking/models.py
@@ -1,36 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
 from about.models import Event
-from .constants import STATUS, TAKEN, TIME_SLOT_OPTIONS
+from .constants import TIME_SLOT_OPTIONS, CAPACITY
 
-
-class TimeSlot(models.Model):
-    time_in = models.CharField(max_length=8, choices=TIME_SLOT_OPTIONS, default="18:00")
-    time_out = models.CharField(max_length=8, choices=TIME_SLOT_OPTIONS, default="18:00")
-
-    def __str__(self):
-        return f"{self.time_in} - {self.time_out}"
-
+# Models revised and refined with help from Mentor Gareth McGirr
 
 class Table(models.Model):
-    number = models.SmallIntegerField()
-    capacity = models.SmallIntegerField()
+    """
+    Model for table creation. Uses constants for predefined timeslots and table sizes. 
+    """
+    number = models.IntegerField(unique=True)
+    capacity = models.IntegerField(choices=CAPACITY)
+
+    class Meta:
+        """
+        Ordering for tables in ascending order.
+        """
+        ordering = ['number']
 
     def __str__(self):
         return f"Table {self.number} - {self.capacity} People" 
 
 
-class TableSlot(models.Model):
-    table_id = models.ForeignKey(Table, on_delete=models.CASCADE, related_name="tables")
-    time_slot_id = models.ForeignKey(TimeSlot, on_delete=models.CASCADE, related_name="time_slot")
-    status = models.IntegerField(choices=TAKEN, default=0)
-
-    def __str__(self):
-        return f"T{self.table_id.number} | {self.time_slot_id}"
-
-
 class Package(models.Model):
-    title = models.CharField(max_length=100)
+    """
+    Model for package creation and reference.
+    """
+    title = models.CharField(max_length=100, unique=True)
     description = models.TextField()
     price = models.CharField(max_length=10, default="£")
 
@@ -39,21 +35,24 @@ class Package(models.Model):
 
 
 class Booking(models.Model):
-    user_id = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="bookings")
-    date = models.DateField(auto_now_add=True)
-    guests = models.SmallIntegerField(default=2)
-    table_id = models.ManyToManyField(TableSlot)
-    time_slot = models.ForeignKey(TimeSlot, on_delete=models.CASCADE, related_name="booking_slot")
-    package = models.ForeignKey(Package, on_delete=models.CASCADE, related_name="package")
-    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="event")
+    """
+    Model for booking creation.
+    """
+    booking_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_guest")
+    booking_date = models.DateField(auto_now_add=True)
+    booking_size = models.IntegerField(default=2)
+    booking_table = models.ForeignKey(Table, on_delete=models.CASCADE, related_name="booking_table")
+    booking_slot = models.IntegerField(choices=TIME_SLOT_OPTIONS, default=1)
+    package = models.ForeignKey(Package, on_delete=models.CASCADE, related_name="booking_package", null=True)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="event", null=True)
     allergies = models.BooleanField(default=False)
     special = models.CharField(max_length=250)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
-    created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        """
+        Ordering for date followed by booking time.
+        """
+        ordering = ['booking_date', 'booking_slot']
 
     def __str__(self):
-        return (f"{self.user_id} | {self.date} | {self.guests} | "
-                f"{self.table_id} | {self.time_slot} | {self.package} | "
-                f"{self.allergies}")
+        return (f"{self.pk} | {self.booking_date} | {self.booking_slot}")
