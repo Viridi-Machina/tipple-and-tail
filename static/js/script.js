254380fe6a96dfd0a2fe8ffc5d1ced79ea77b33f
jacen green <jacen.l.green@hotmail.com>
2024-07-02 06:18:04 +0100

    FEAT: finalise styling for booking pages

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,1 @@
-// django route for javascript to parse logged-in username from project
-const user_id = JSON.parse(document.getElementById('username').textContent);
 
-// Query constants
-const nameField = document.getElementById("id_booking_name");
-const allergyLabel = document.getElementById("id_allergies");
-
-nameField.setAttribute("value", user_id);
